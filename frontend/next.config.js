0f73dd685483252f3e634c503b2c2f0d665b0758
ravix9145 <ravix9145@gmail.com>
2026-08-23 14:52:35 +0530

    Update API destination to use environment variable

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8000/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
       },
     ];
   },
